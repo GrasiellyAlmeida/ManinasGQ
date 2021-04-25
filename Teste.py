3f23165f576c4078908b13a07573a79808af3523
Quézia Moura <queziapsmoura@gmail.com>
2021-04-25 19:38:40 -0300

    Quézia e Grasi

--- Teste.py
+++ Teste.py
@@ -15,3 +15,26 @@ def extrai_valor(carta):
         return '10'
     else:
         return carta[0]
+
+
+def lista_movimentos_possiveis(baralho):
+    if baralho[0]:
+        return []
+    else:
+        
+      for i in range(1,len(baralho)):
+
+        if (baralho[i] == '10' and baralho[i - 1] == '10') or (baralho[i + 2] == '10' and baralho[i - 1] == '10'):
+          return [1]
+
+        valor1 = baralho[i-1][0]
+        naipe1 = baralho[i-1][1]
+
+        valor3 = baralho[i-1][0]
+        naipe3 = baralho[i-1][1]
+
+
+        else:
+
+          (baralho[i] == valor1 or baralho[i] == naipe1) or (baralho[i + 2] == valor3 or baralho[i + 2] == naipe3)
+          return [1]
