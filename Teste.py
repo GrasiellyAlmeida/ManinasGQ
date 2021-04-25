a771938af11ef531082cf23457c630bb19c6f227
Quézia Moura <queziapsmoura@gmail.com>
2021-04-25 16:56:15 -0300

    Quézia and Grasi

--- Teste.py
+++ Teste.py
@@ -1,8 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 21 20:40:28 2021
+from random import shuffle
+def cria_baralho():
+    cartas = ['A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♠', 'Q♠', 'K♠', 'J♥', 'Q♥', 'K♥', 'J♦', 'Q♦', 'K♦', 'J♣', 'Q♣', 'K♣']
+    lista = shuffle(cartas)
+    return cartas
+ 
+    if carta == '10♣' or carta == '10♥' or carta == '10♠' or carta == '10♦':
+        return carta[2]
+    else:
+        return carta[1]
 
-@author: quezi
-"""
-
-print("Boa Noite!!")
+def extrai_valor(carta):
+    if carta == '10♣' or carta == '10♥' or carta == '10♠' or carta == '10♦':
+        return '10'
+    else:
+        return carta[0]
